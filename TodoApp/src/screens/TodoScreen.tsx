77d95f32c503fe22f53db656ee4136e0fd8419a2
lynnetteeee <lynnette.oxh@gmail.com>
2024-05-09 14:09:44 +0800

    Fix TodoScreen functionalities

--- TodoApp/src/screens/TodoScreen.tsx
+++ TodoApp/src/screens/TodoScreen.tsx
@@ -12,18 +12,72 @@ import {useRoute} from '@react-navigation/native';
 
 // placeholder for actual styles
 const styles = StyleSheet.create({
+  listHeader: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 5,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 10,
+    paddingHorizontal: 10,
+  },
+  modalView: {
+    marginTop: '50%',
+    marginHorizontal: 50,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
     alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    padding: 5,
+    color: 'black',
+  },
+  inputDescription: {
+    marginBottom: 15,
+    alignSelf: 'flex-start',
+    paddingLeft: 5,
+    color: '#a2a3a2',
+    fontSize: 12,
+  },
+  taskHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginTop: 10,
+    borderRadius: 10,
+    backgroundColor: '#daebf2',
+    elevation: 3,
+  },
+  taskDescription: {
+    color: '#255263',
+    paddingLeft: 15,
+    paddingVertical: 5,
+    fontSize: 18,
   },
   taskText: {
-    color: '255263',
-    fontSize: 18,
-    flex: 1,
+    color: '#255263',
   },
   taskTextDone: {
-    fontSize: 18,
     textDecorationLine: 'line-through',
     color: 'gray',
   },
@@ -74,11 +128,12 @@ const TodoScreen = () => {
     );
   };
 
-  const taskHeader = tasks.length > 0 ? <Text>Tasks:</Text> : null;
+  const taskHeader =
+    tasks.length > 0 ? <Text style={styles.taskHeader}>Tasks:</Text> : null;
 
   return (
     <View style={styles.container}>
-      <Text>{listTitle}</Text>
+      <Text style={styles.listHeader}>{listTitle}</Text>
       <Button
         color="#2f80a1"
         title="Add New Task +"
@@ -86,11 +141,16 @@ const TodoScreen = () => {
       />
       {taskHeader}
       {tasks.map(task => (
-        <View key={task.id}>
-          <Text style={task.is_done ? styles.taskTextDone : styles.taskText}>
-            {task.description}
-          </Text>
-          <TouchableOpacity onPress={() => handleCompleteTask(task.id)} />
+        <View key={task.id} style={styles.buttonContainer}>
+          <TouchableOpacity onPress={() => handleCompleteTask(task.id)}>
+            <Text
+              style={[
+                styles.taskDescription,
+                task.is_done ? styles.taskTextDone : styles.taskText,
+              ]}>
+              {task.description}
+            </Text>
+          </TouchableOpacity>
         </View>
       ))}
       <Modal
@@ -100,13 +160,14 @@ const TodoScreen = () => {
         onRequestClose={() => {
           setModalVisible(false);
         }}>
-        <View>
+        <View style={styles.modalView}>
           <TextInput
+            style={styles.input}
             placeholder="Enter task description"
             onChangeText={text => setNewTaskDescription(text)}
             value={newTaskDescription}
           />
-          <Button title="Add Task" onPress={handleAddTask} />
+          <Button title="Add Task +" onPress={handleAddTask} color="#2f80a1" />
         </View>
       </Modal>
     </View>
